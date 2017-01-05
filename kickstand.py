43e4877c437c63cf3c31ce818c49dd1299bb5061
Normand Cyr <normand.cyr@gmail.com>
2017-01-05 08:35:33 -0500

    code clean up

--- kickstand.py
+++ kickstand.py
@@ -11,17 +11,19 @@ def kickstand(bot, trigger):
 
 # Check if there is a new comic strip today
     if request.status_code == 200:
-#       print('Web site exists')
+#        print('Web site exists')
         comicurl = comicbaseaddress + today + '/'
 # If yes, print the link
-#       print('The address is ' + comicurl)
-# Print the title
+#        print('The address is ' + comicurl)
+
+# And print the title of the comic
         html = BeautifulSoup(request.text, 'html.parser')
         fulltitlenodate = html.title.text[11:]
         where_dash = fulltitlenodate.find(' - ')
         if where_dash == -1:
             bot.say(fulltitlenodate + ' ' + comicurl)
         bot.say('Nouveau Kickstand Comics: ' + fulltitlenodate[:where_dash] + ' | ' + comicurl)
+
 # If no, do nothing
     else:
         bot.say('Pas de Kickstand Comics aujourd\'hui.') 
