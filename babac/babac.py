142990360943f205b13d2306b59651b3fe61c4e0
Normand Cyr <normand.cyr@gmail.com>
2017-01-26 12:58:08 -0500

    code clean up to remove unused commented lines

--- babac/babac.py
+++ babac/babac.py
@@ -6,19 +6,10 @@ from sopel import web
 from bs4 import BeautifulSoup
 import cookielib
 import mechanize
-#import sys
 import yaml
 
 def get_query(bot, trigger):
-    #query = str(sys.argv[1])
     query = trigger.group(2)
-    if query != None:
-        query = query.replace(' ', '+')
-        bot.say('Searching in the Babac catalog for: %s' % query)
-    else:
-        return bot.reply('.babac what? Please specify your query. For example ".babac training wheels"')
-        #return '.babac what? Please specify your query. For example ".babac training wheels"'
-        #print('Searching in the Babac catalog for: %s' % query)
 
     return query
 
@@ -72,9 +63,7 @@ def search_item(br, query):
 def print_results(bot, br, itemsfound):
     if len(itemsfound)>0:
         bot.say('Returning %i items.' % len(itemsfound))
-        #print('Returning %i items.' % len(itemsfound))
         bot.say('#Babac | Item name and price' )
-        #print('#Babac | Item name and price' )
         for itemname in itemsfound:
             shortitemname = itemname.contents[1].string
             for itemlink in itemname.find_all('a'):
@@ -85,15 +74,21 @@ def print_results(bot, br, itemsfound):
                 price = soupitempagetext.find('meta', itemprop='price')
                 pricenumber = float(str(price[u'content']))
             bot.say(skushort + ' | ' + shortitemname + ' | ' + '%.2f'% (pricenumber) + '$')
-            #print(skushort + ' | ' + shortitemname + ' | ' + '%.2f'% (pricenumber) + '$')
     else:
         bot.say('No product found :(')
-        #print('No product found :(')
 
 @commands('babac')
 @example('.babac training wheels')
 def babac(bot, trigger):
     query = get_query(bot, trigger)
+
+    if query != None:
+        query = query.replace(' ', '+')
+        bot.say('Searching in the Babac catalog for: %s' % query)
+    else:
+        return bot.reply('.babac what? Please specify your query. For example ".babac training wheels"')
+        exit(0)
+
     username, password = load_config()
     br = login(username, password)
     itemsfound = search_item(br, query)
